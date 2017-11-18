9e4e6dcf6fdf77691fc1fee9aa3458fe46034ded
Aaron Sarnat <accounts@pixelday.com>
2017-11-17 17:50:08 -0800

    making demo page presentable

--- js/treeData.js
+++ js/treeData.js
@@ -305,3 +305,407 @@ const introData = {
     }]
   }
 };
+
+const exampleData = {
+  "text": "ACME, Inc. Org Chart",
+  "nodeTypeToStyle": {
+    "root": ["color1", "strong"],
+    "1stlevel": ["color2"],
+    "2ndlevel": ["color3"],
+    "3rdlevel": ["color4"],
+    "4thlevel": ["color5"]
+  },
+  "linkToPosition": {
+    "Executive Assistant": "right",
+    "Sr. Project Manager": "right",
+    "Project Manager": "right",
+    "Architect": "right",
+    "Sr. Software Engineer": "right",
+    "Software Engineer": "right",
+    "Sr. Front-End Engineer": "right",
+    "Front-End Engineer": "right",
+    "Sr. IT Technician": "right",
+    "IT Technician": "right",
+    "Sr. UX Designer": "right",
+    "Visual Designer": "right",
+    "Sr. Interaction Designer": "right",
+    "Interaction Designer": "right",
+    "Illustrator": "right",
+    "Photographer": "right",
+    "3D Artist": "right"
+  },
+  "linkNameToLabel": {
+    "Executive Assistant": "EA",
+    "Sr. Project Manager": "SR. PM",
+    "Project Manager": "PM",
+    "Architect": "Arch",
+    "Sr. Software Engineer": "Sr. SE",
+    "Software Engineer": "SE",
+    "Sr. Front-End Engineer": "Sr. FE",
+    "Front-End Engineer": "FE",
+    "Sr. IT Technician": "Sr. ITT",
+    "IT Technician": "ITT",
+    "Sr. UX Designer": "Sr. UX",
+    "Visual Designer": "VD",
+    "Sr. Interaction Designer": "Sr. ID",
+    "Interaction Designer": "ID",
+    "Illustrator": "Ill",
+    "Photographer": "PH",
+    "3D Artist": "3D"
+  },
+  "root": {
+    "word": "Esther Walker",
+    "attributes": ["CEO"],
+    "nodeType": "root",
+    "children": [
+      {
+        "word": "Frances Peterson",
+        "link": "Executive Assistant",
+        "attributes": ["Executive Assistant"],
+        "nodeType": "1stlevel"
+      },
+      {
+        "word": "Ernesto Daniel",
+        "link": "COO",
+        "nodeType": "1stlevel",
+        "children": [
+          {
+            "word": "Lamar Saunders",
+            "link": "Controller",
+            "nodeType": "2ndlevel"
+          },
+          {
+            "word": "Alma Vargas",
+            "link": "Office Manager",
+            "nodeType": "2ndlevel"
+          },
+          {
+            "word": "Nichole Lindsey",
+            "link": "HR Manager",
+            "nodeType": "2ndlevel"
+          },
+          {
+            "word": "Erma Austin",
+            "link": "Sr. Program Manager",
+            "nodeType": "2ndlevel",
+            "children": [
+              {
+                "word": "Lowell Shelton",
+                "link": "Sr. Project Manager",
+                "nodeType": "3rdlevel",
+                "attributes": ["Sr. Project Manager"]
+              },
+              {
+                "word": "Victor Kim",
+                "link": "Project Manager",
+                "nodeType": "3rdlevel",
+                "attributes": ["Project Manager"]
+              },
+              {
+                "word": "Rodney Fowler",
+                "link": "Project Manager",
+                "nodeType": "3rdlevel",
+                "attributes": ["Project Manager"]
+              },
+              {
+                "word": "Leah Fitzgerald",
+                "link": "Project Manager",
+                "nodeType": "3rdlevel",
+                "attributes": ["Project Manager"]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "word": "Kristen Vasquez",
+        "link": "CTO",
+        "nodeType": "1stlevel",
+        "children": [
+          {
+            "word": "Calvin Francis",
+            "link": "Director of Engineering",
+            "nodeType": "2ndlevel",
+            "children": [
+              {
+                "word": "Sonya Hansen",
+                "link": "Architect",
+                "attributes": ["Architect"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Orville Mcdaniel",
+                "link": "Sr. Software Engineer",
+                "attributes": ["Sr. Software Engineer"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Steven Copeland",
+                "link": "Sr. Software Engineer",
+                "attributes": ["Sr. Software Engineer"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Freda Reese",
+                "link": "Software Engineer",
+                "attributes": ["Software Engineer"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Wallace Matthews",
+                "link": "Software Engineer",
+                "attributes": ["Software Engineer"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Mack Baker",
+                "link": "Software Engineer",
+                "attributes": ["Software Engineer"],
+                "nodeType": "3rdlevel"
+              }
+            ]
+          },
+          {
+            "word": "Mary Barnett",
+            "link": "Director of Engineering",
+            "nodeType": "2ndlevel",
+            "children": [
+              {
+                "word": "Darryl Patterson",
+                "link": "Sr. Software Engineer",
+                "attributes": ["Sr. Software Engineer"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Joy Coleman",
+                "link": "Sr. Front-End Engineer",
+                "attributes": ["Sr. Front-End Engineer"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Austin Rodriquez",
+                "link": "Front-End Engineer",
+                "attributes": ["Front-End Engineer"],
+                "nodeType": "3rdlevel"
+              }
+            ]
+          },
+          {
+            "word": "Terrell Benson",
+            "link": "Director of IT",
+            "nodeType": "2ndlevel",
+            "children": [
+              {
+                "word": "Amber Curtis",
+                "link": "Sr. IT Technician",
+                "attributes": ["Sr. IT Technician"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Jermaine Caldwell",
+                "link": "IT Technician",
+                "attributes": ["IT Technician"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Emma Hodges",
+                "link": "IT Technician",
+                "attributes": ["IT Technician"],
+                "nodeType": "3rdlevel"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "word": "Gwendolyn Jimenez",
+        "link": "VP of Design",
+        "nodeType": "1stlevel",
+        "children": [
+          {
+            "word": "Vivian Santos",
+            "link": "Creative Director",
+            "nodeType": "2ndlevel",
+            "children": [
+              {
+                "word": "Rose Morrison",
+                "link": "Associate Creative Director",
+                "nodeType": "3rdlevel",
+                "children": [
+                  {
+                    "word": "Bridget Curry",
+                    "link": "Sr. UX Designer",
+                    "attributes": ["Sr. UX Designer"],
+                    "nodeType": "4thlevel"
+                  },
+                  {
+                    "word": "Ira Black",
+                    "link": "Visual Designer",
+                    "attributes": ["Visual Designer"],
+                    "nodeType": "4thlevel"
+                  }
+                ]
+              },
+              {
+                "word": "Tyler Manning",
+                "link": "Associate Creative Director",
+                "nodeType": "3rdlevel",
+                "children": [
+                  {
+                    "word": "Kim Morton",
+                    "link": "Sr. UX Designer",
+                    "attributes": ["Sr. UX Designer"],
+                    "nodeType": "4thlevel"
+                  },
+                  {
+                    "word": "Eddie Moran",
+                    "link": "Sr. Interaction Designer",
+                    "attributes": ["Sr. Interaction Designer"],
+                    "nodeType": "4thlevel"
+                  },
+                  {
+                    "word": "Saul Green",
+                    "link": "Interaction Designer",
+                    "attributes": ["Interaction Designer"],
+                    "nodeType": "4thlevel"
+                  },
+                  {
+                    "word": "Earnest Sandoval",
+                    "link": "Visual Designer",
+                    "attributes": ["Visual Designer"],
+                    "nodeType": "4thlevel"
+                  },
+                  {
+                    "word": "Wendy Long",
+                    "link": "Illustrator",
+                    "attributes": ["Illustrator"],
+                    "nodeType": "4thlevel"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "word": "Jessie Warren",
+            "link": "Art Director",
+            "nodeType": "2ndlevel",
+            "children": [
+              {
+                "word": "Joey Joseph",
+                "link": "Photographer",
+                "attributes": ["Photographer"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Everett Boone",
+                "link": "Illustrator",
+                "attributes": ["Illustrator"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Michelle Colon",
+                "link": "Illustrator",
+                "attributes": ["Illustrator"],
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Tommie Larson",
+                "link": "3D Artist",
+                "attributes": ["3D Artist"],
+                "nodeType": "3rdlevel"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "word": "Jerry Rodriguez",
+        "link": "VP of Marketing",
+        "nodeType": "1stlevel",
+        "children": [
+          {
+            "word": "Yolanda Butler",
+            "link": "Sr. Marketing Manager",
+            "nodeType": "2ndlevel"
+          },
+          {
+            "word": "Lorraine Barnes",
+            "link": "Sr. Marketing Manager",
+            "nodeType": "2ndlevel"
+          },
+          {
+            "word": "Leo Gibbs",
+            "link": "Marketing Manager",
+            "nodeType": "2ndlevel"
+          },
+          {
+            "word": "Lillian Edwards",
+            "link": "Marketing Assistant",
+            "nodeType": "2ndlevel"
+          }
+        ]
+      },
+      {
+        "word": "Natasha Hudson",
+        "link": "VP of Product",
+        "nodeType": "1stlevel",
+        "children": [
+          {
+            "word": "Jill Munoz",
+            "link": "Product Director",
+            "nodeType": "2ndlevel",
+            "children": [
+              {
+                "word": "Anthony Drake",
+                "link": "Sr. Product Manager",
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Edward Thornton",
+                "link": "Sr. Product Manager",
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Benjamin Parker",
+                "link": "Product Manager",
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Sandy Holloway",
+                "link": "Product Manager",
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Louis Powell",
+                "link": "Product Manager",
+                "nodeType": "3rdlevel"
+              }
+            ]
+          },
+          {
+            "word": "Tamara Phillips",
+            "link": "Product Director",
+            "nodeType": "2ndlevel",
+            "children": [
+              {
+                "word": "Felipe Shaw",
+                "link": "Sr. Product Manager",
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Jeremy Roberts",
+                "link": "Product Manager",
+                "nodeType": "3rdlevel"
+              },
+              {
+                "word": "Shaun Poole",
+                "link": "Product Manager",
+                "nodeType": "3rdlevel"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+};
